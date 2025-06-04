66c8de0da8691d85acc4285fa8e04cb2eefefa7a
ahmetbostanciklioglu <ahmetbostancikli@gmail.com>
2025-06-04 21:16:01 +0300

    Initial Commit

---
+++ iOS-Academy-Client/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS-Academy-Client
+//
+//  Created by Ahmet Bostanci on 4.06.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS-Academy-Client/iOS_Academy_ClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Academy_ClientApp.swift
+//  iOS-Academy-Client
+//
+//  Created by Ahmet Bostanci on 4.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Academy_ClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
